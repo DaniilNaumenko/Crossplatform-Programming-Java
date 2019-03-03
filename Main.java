586fdc74998eeaae402403474b8c762353239662
DaniilNaumenko <47709804+DaniilNaumenko@users.noreply.github.com>
2019-03-03 13:10:51 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main (String[]argc){
-        SimpleGUI app = new SimpleGUI();
-        app.setVisible(true);
-        app.setResizable(false);
-
-    }
-}
